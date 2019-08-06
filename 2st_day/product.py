9757c5b0c95e5cfbd6f0e111c650519ca120a513
pr1s0n <li1111111@vip.qq.com>
2019-08-06 16:54:03 +0800

    procduct

---
+++ 2st_day/product.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+def product(*nums):
+    if not nums:
+        raise TypeError('请至少输入一个值')
+    sum = 1
+    for num in nums:
+        sum = sum * num
+    return sum
+print('product(5) =', product(5))
+print('product(5, 6) =', product(5, 6))
+print('product(5, 6, 7) =', product(5, 6, 7))
+print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
+if product(5) != 5:
+    print('测试失败!')
+elif product(5, 6) != 30:
+    print('测试失败!')
+elif product(5, 6, 7) != 210:
+    print('测试失败!')
+elif product(5, 6, 7, 9) != 1890:
+    print('测试失败!')
+else:
+    try:
+        product()
+        print('测试失败!')
+    except TypeError:
+        print('测试成功!success')
