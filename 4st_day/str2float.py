f3e1649168ac3ad68ecff8b2c45bdc85a8491c54
pr1s0n <li1111111@vip.qq.com>
2019-08-08 17:48:58 +0800

    str2float

--- 4st_day/str2float.py
+++ 4st_day/str2float.py
@@ -2,19 +2,14 @@ from functools import reduce
 def str2float(s):
 	s_1 = s[:s.index('.')]
 	s_2 = s[s.index('.')+1:]
-
 	def fn(x, y):
 
 		return x * 10 + y
-
 	def char2num(h):
 		digits = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
 		return digits[h]
-
 	s1 = reduce(fn, map(char2num,s_1))
-
 	s2 = reduce(fn, map(char2num,s_2))
-
 	return s1 + s2 / 10 ** len(s_2)
 print('str2float(\'123.456\') =', str2float('123.456'))
 if abs(str2float('123.456') - 123.456) < 0.00001:
