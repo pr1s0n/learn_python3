67d4a236d8e1e4a9c013b080925f0a1ba32aa9a4
pr1s0n <li1111111@vip.qq.com>
2019-08-10 18:17:46 +0800

    类继承及调用

---
+++ 6st_day/classes.py
@@ -0,0 +1,14 @@
+class Animal(object):
+	"""docstring for Animal"""
+	def __init__(self):
+		pass
+	def run(self):
+		print("Animal is running...")
+
+class Dog(Animal):
+	"""docstring for Dog"""
+	def __init__(self):
+		pass
+dog = Dog()
+dog.run()
+		
