b1ef1574d9cdb68813b1e501d7109e8024aa77bd
pr1s0n <li1111111@vip.qq.com>
2019-08-12 15:47:29 +0800

    screen类演示只读和读写两种应用方式

---
+++ 8st_day/screen.py
@@ -0,0 +1,26 @@
+class Screen(object):
+	"""docstring for Screen"""
+	@property
+	def width(self):
+		return self._width
+	@width.setter
+	def width(self,value):
+		self._width = value
+
+	@property
+	def height(self):
+		return self._height
+	@height.setter
+	def height(self,value):
+		self._heigth = value
+	@property
+	def resolution(self):
+		return self._heigth * self._width
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution =', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
+else:
+    print('测试失败!')
