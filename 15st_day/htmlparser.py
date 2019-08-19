f034398ca979bfb05574b0374d51de75a1542dc0
pr1s0n <li1111111@vip.qq.com>
2019-08-19 21:06:13 +0800

    htmlparser实例

---
+++ 15st_day/htmlparser.py
@@ -0,0 +1,25 @@
+from urllib import request
+from html.parser import HTMLParser
+class HtmlParserTest(HTMLParser):
+	"""docstring for HtmlParserTest"""
+	def __init__(self):
+		super().__init__()
+		self.tag = None
+	def handle_starttag(self,tag,attrs):
+		if tag == 'time':
+			self.tag = 'Time:'
+		elif('class','event-location') in attrs:
+			self.tag = 'Location:'
+		elif('class','event-title') in attrs:
+			self.tag = '\nTitle:'
+	def handle_data(self,data):
+		if self.tag:
+			print(self.tag,data)
+	def handle_endtag(self,tag):
+		self.tag = None
+
+with request.urlopen('https://www.python.org/events/python-events/') as f:
+	html_data = f.read().decode('utf-8')
+
+parser = HtmlParserTest()
+parser.feed(html_data)
